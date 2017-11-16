19f249f7e1b842aa4e3d15edc6a2dcbd17fa4ee8
tlongo21 <tlongo1@villanova.edu>
2017-11-16 16:27:00 -0500

    init git ignore

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cavemen
+Programming special relativity in the CAVE.
+
+## How to Contribute
+First, fork the project to your own GitHub account. This only needs to be done once. Then, and this is the most important step, **create a branch** from the master repository. **Do not make changes directly to the master**.
+
+## TODO
+This is where we will store important items which can be checked off next.
+- [x] Get everyone set up with GitHub
+- [ ] Get everyone set up with Unity
+- [ ] Get Unity set up with GitHub
+- [ ] Symplectic Integrators
+	- [ ] Create symplectic integrator for free-fall (no air resistance)
+	- [ ] Create symplectic integrator for double-pendulum problem
+	- [ ] Create a symplectic integrator for central-force systems
---
+++ unity/Assets/Scripts/Test1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Vector3 velocityVector; //average velocity this FixedUpdata()
+    public Vector3 netForceVector; 
+    public List<Vector3> forceVectorList = new List<Vector3>();
+
+	void FixedUpdate ()
+    {
+        SumForces();
+        UpdateVelocity();
+
+        transform.position += velocityVector * Time.deltaTime;
+
+	}
+    void SumForces()
+    {
+        netForceVector = Vector3.zero;
+        foreach (Vector3 forceVector in forceVectorList)
+        {
+            netForceVector = netForceVector + forceVector;
+        }
+    }
+    void UpdateVelocity()
+    {
+        Vector3 accelerationVector = netForceVector / rb.mass;
+        velocityVector += accelerationVector * Time.deltaTime;
+    }
+}
