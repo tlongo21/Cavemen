9f5c05abb481996a25dc902a49781703e68d8c7b
tlongo21 <tlongo1@villanova.edu>
2017-11-18 19:26:59 -0500

    scripts with comments

--- unity/Assets/Scripts/CameraController.cs
+++ unity/Assets/Scripts/CameraController.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// this is a program that controls the motion of the camera
 public class CameraController : MonoBehaviour {
-
+    // it requires a GameObject that will be defined as the player
     public GameObject player;
 
+    // we will define a vector called offset to house a constant location for the camera
     private Vector3 offset;
 
+    // 
     void Start ()
     {
         offset = transform.position - player.transform.position;
     }
-    
+
     void LateUpdate ()
     {
         transform.position = player.transform.position + offset;
---
+++ unity/Assets/Scripts/moveCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour {
+
+    public Vector3 speed;     //this will be the speed in the x-direction, added by user
+
+
+    void FixedUpdate()
+    {
+        Vector3 transform.position = speed / Time.deltaTime;
+    }
+}
