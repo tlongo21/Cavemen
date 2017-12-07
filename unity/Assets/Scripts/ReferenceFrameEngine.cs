7c4bf675be6a997d72a7d5ffa563ce7c24878869
tlongo21 <tlongo1@villanova.edu>
2017-12-07 12:56:14 -0500

    samll edits

--- unity/Assets/Scripts/ReferenceFrameEngine.cs
+++ unity/Assets/Scripts/ReferenceFrameEngine.cs
@@ -38,6 +38,12 @@ public class ReferenceFrameEngine : MonoBehaviour {
         SetSpeedText(); // Call our UI update function.
     }
 
+    private void OnEnable()
+    {
+        
+    }
+}
+
     private Vector3 minSize = Vector3.zero;
     private Vector3 normSize = new Vector3 (5f,5f,5f);
 
@@ -54,8 +60,24 @@ public class ReferenceFrameEngine : MonoBehaviour {
 
         SetSpeedText();
 
-        float currentXSpeed = Mathf.Abs(V_x);
-        float currentZSpeed = Mathf.Abs(V_z);        
+        // On the start up, initialize the variables.
+        refFrame = GameObject.Find("Main Camera");
+        refFrameObj = GetComponent<FreeCamera>();
+
+        // Use the velocity vector determined from the Physics Engine
+        refFrameSpeed = refFrameObj.get_Speed();
+        print(refFrameSpeed);
+        refFrameDirection = refFrameObj.get_direction_angle();
+
+        float V_ref_x = refFrameSpeed * Mathf.Sin(refFrameDirection);
+        float V_ref_z = refFrameSpeed * Mathf.Cos(refFrameDirection);
+
+        float currentXSpeed = Mathf.Abs(V_ref_x);
+        float currentZSpeed = Mathf.Abs(V_ref_z);
+
+        float scaleX_adj = Mathf.Sqrt(1 - Mathf.Pow(currentXSpeed / lightSpeed, 2f));
+        float scaleZ_adj = Mathf.Sqrt(1 - Mathf.Pow(currentZSpeed / lightSpeed, 2f));
+        this.transform.localScale = new Vector3(scaleX_adj * normSize.x, normSize.y, scaleZ_adj * normSize.z);     
     }
    
     // Update the speed counter display that the player will view.
